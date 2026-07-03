489c046f504c12101d2be2c1a0b8a7c95c085523
Maheesha Udalagama <maheeshaudalagama@gmail.com>
2026-07-03 22:19:52 +0530

    Create ProductService session bean

---
+++ src/main/java/com/techmart/service/ProductService.java
@@ -0,0 +1,47 @@
+package com.techmart.service;
+
+import com.techmart.entity.Product;
+import com.techmart.repository.ProductRepository;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import java.util.List;
+
+@Stateless
+public class ProductService {
+
+    @EJB
+    private ProductRepository productRepository;
+
+    public void addProduct(Product product) {
+        productRepository.create(product);
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.find(id);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+    
+    public List<Product> getActiveProducts() {
+        return productRepository.findActiveProducts();
+    }
+
+    public List<Product> getProductsByCategory(String category) {
+        return productRepository.findByCategory(category);
+    }
+
+    public void updateProduct(Product product) {
+        productRepository.edit(product);
+    }
+    
+    public void deactivateProduct(Long id) {
+        Product product = productRepository.find(id);
+        if (product != null) {
+            product.setActive(false);
+            productRepository.edit(product);
+        }
+    }
+}
