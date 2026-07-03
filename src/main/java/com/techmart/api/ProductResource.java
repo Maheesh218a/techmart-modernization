eaab6285801cf714332e67e6eb52bf565dcc5a3b
Maheesha Udalagama <maheeshaudalagama@gmail.com>
2026-07-03 22:25:37 +0530

    Create ProductResource REST API

---
+++ src/main/java/com/techmart/api/ProductResource.java
@@ -0,0 +1,46 @@
+package com.techmart.api;
+
+import com.techmart.entity.Product;
+import com.techmart.service.ProductService;
+
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/products")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ProductResource {
+
+    @EJB
+    private ProductService productService;
+
+    @GET
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GET
+    @Path("/active")
+    public List<Product> getActiveProducts() {
+        return productService.getActiveProducts();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getProduct(@PathParam("id") Long id) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return Response.ok(product).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @POST
+    public Response createProduct(Product product) {
+        productService.addProduct(product);
+        return Response.status(Response.Status.CREATED).entity(product).build();
+    }
+}
